2c6d37986ce60734e7cc0db579c6805f4f42101a
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2020-05-24 15:15:18 +0530

    Changed docs

--- docs/dist/build/js/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("open").addEventListener('click', () => {
-    document.getElementsByClassName('slider-nav')[0].classList.add('slider-show');
-});
-document.getElementById("close").addEventListener('click', () => {
-    document.getElementsByClassName('slider-nav')[0].classList.remove('slider-show');
-});
