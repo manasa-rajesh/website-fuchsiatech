723d3fd35b4dba30d1fe0112c16a19a794bd11f4
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2020-05-29 19:36:37 +0530

    Changed landing, contact, sidenavbar and main navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 
-window.onload = (event) => {
+window.onload = () => {
     show();
 };
 function show(){
@@ -11,44 +11,9 @@ function show(){
 
 document.getElementById("open").addEventListener('click', () => {
     document.getElementsByClassName('sliding-nav-outlet')[0].classList.add('slider-show');
+    document.getElementsByClassName('dim-background-on-menu')[0].classList.add('show-dimmed-background');
 });
 document.getElementById("close").addEventListener('click', () => {
     document.getElementsByClassName('sliding-nav-outlet')[0].classList.remove('slider-show');
-});
-
-document.getElementsByTagName('form')[0].addEventListener('submit', (e) => {
-    e.preventDefault();
-    document.getElementById('submit').innerHTML = `<div><div class="loader"></div></div>`;
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST','https://fuchsiatech.herokuapp.com/');
-    xhr.setRequestHeader('Content-type','application/json');
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-            try {
-                let x = JSON.parse(xhr.responseText);
-                if (x.success == false) {
-                    document.getElementById('submit').innerHTML = `<div>Send</div>`
-                    document.getElementById('error').innerText = x.message;
-                } else {
-                    document.getElementById('submit').innerHTML = `<div>Sent</div>`
-                    setTimeout(() => {
-                        document.getElementById('submit').innerHTML = `<div>Send</div>`
-                    }, 2000);
-                }
-            } catch(err) {
-                document.getElementById('submit').innerHTML = `<div>Send</div>`
-                document.getElementById('error').innerText = 'Error sending your email. Please try again';
-            }
-            setTimeout(() => {
-                document.getElementById('error').innerHTML = ``
-            }, 2000);
-        }
-    }
-    xhr.send(JSON.stringify({email: document.getElementById('email').value, text: document.getElementById('message').value, subject:document.getElementById('subject').value, name: document.getElementById('name').value }));
-
-    setTimeout(() => {
- 
-    }, 3000);
-
+    document.getElementsByClassName('dim-background-on-menu')[0].classList.remove('show-dimmed-background');
 });
